b80f0313b497aa25ff770c6c6a8254cf0ec70333
valeriano alexander <valerianoalexander@gmail.com>
2024-01-03 17:11:29 +0100

    02. Create my First Test Case and Run Two ways UI or CLI

---
+++ cypress/e2e/myFirstTest.cy.js
@@ -0,0 +1,18 @@
+//** describe comes from mocha  **//
+describe("first test suite", () => {
+  it("test1", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.title().should("eq", "OrangeHRM");
+  });
+  it("test2", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+    cy.title().should("eq", "OrangeHRM123");
+  });
+  it("test3", () => {
+    expect(true).to.equal(true);
+  });
+});
