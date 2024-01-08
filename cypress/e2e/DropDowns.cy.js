8810db5976466911c707056d2fb9acd2ed9404cc
valeriano alexander <valerianoalexander@gmail.com>
2024-01-08 16:40:05 +0100

    08. Auto suggest drop downs

--- cypress/e2e/DropDowns.cy.js
+++ cypress/e2e/DropDowns.cy.js
@@ -15,10 +15,15 @@ describe("handle dropdowns", () => {
   });
 
   it.skip("Dropdown without select", () => {
-    cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
-    cy.get("#dropdown-class-example").should("be.visible");
-    cy.get("#dropdown-class-example")
-      .select("Option1")
-      .should("contain", "Option1");
+    cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/");
+    cy.get("#select2-billing_country-container").click();
+    cy.get("input.select2-search__field").type("Peru").type("{enter}");
+    cy.get("#select2-billing_country-container").should("have.text", "Peru");
+  });
+
+  it("Auto suggest dropdown", () => {
+    cy.visit("https://www.wikipedia.org");
+    cy.get("#searchInput").type("Delhi");
+    cy.get(".suggestion-title").contains("Delhi Capitals").click();
   });
 });
