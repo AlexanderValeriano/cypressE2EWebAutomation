e96410c53239b4b9720eadd5411c014f4ec45fcf
valeriano alexander <valerianoalexander@gmail.com>
2024-01-11 18:18:04 +0100

    16. Fixtures and DDT | Access through Hook - for multiple it blocks

--- cypress/e2e/Fixtures.cy.js
+++ cypress/e2e/Fixtures.cy.js
@@ -1,6 +1,6 @@
 describe("", () => {
   // Direct Acces
-  it.only("FixturesDemoTest", () => {
+  /*  it("FixturesDemoTest", () => {
     cy.visit(
       "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
     );
@@ -12,7 +12,27 @@ describe("", () => {
         ".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module"
       ).should("have.text", data.expected);
     });
-  });
+  }); */
+
   // Access through Hook - for multiple it blocks
-  it("FixturesDemoTest", () => {});
+  let userdata;
+  before(() => {
+    cy.fixture("orangehrm").then((data) => {
+      userdata = data;
+    });
+  });
+
+  it("FixturesDemoTest", () => {
+    cy.visit(
+      "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login"
+    );
+
+    cy.get("input[placeholder='Username']").type(userdata.username);
+    cy.get("input[placeholder='Password']").type(userdata.password);
+    cy.get("button[type='submit']").click();
+    cy.get(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module").should(
+      "have.text",
+      userdata.expected
+    );
+  });
 });
