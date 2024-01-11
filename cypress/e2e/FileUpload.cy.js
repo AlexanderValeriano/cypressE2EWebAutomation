42bb30d160de46b046d57d3158f3696d5845b2a4
valeriano alexander <valerianoalexander@gmail.com>
2024-01-11 17:34:48 +0100

    14. Handling file upload | Shadow DOM

--- cypress/e2e/FileUpload.cy.js
+++ cypress/e2e/FileUpload.cy.js
@@ -8,7 +8,7 @@ describe("File Uploads", () => {
     cy.wait(3000);
     cy.get("div[class='example'] h3").should("have.text", "File Uploaded!");
   });
-  it.only("File upload - Rename", () => {
+  it("File upload - Rename", () => {
     cy.visit("https://the-internet.herokuapp.com/upload");
     cy.get("#file-upload").attachFile({
       filePath: "oneMegaPDF.pdf",
@@ -19,12 +19,41 @@ describe("File Uploads", () => {
     cy.get("div[class='example'] h3").should("have.text", "File Uploaded!");
     cy.get("#uploaded-files").contains("myFile.pdf");
   });
-  it("Single File upload", () => {
+  it("File upload - Drag and Drop", () => {
     cy.visit("https://the-internet.herokuapp.com/upload");
-    cy.get("#file-upload");
+    cy.get("#drag-drop-upload").attachFile("oneMegaPDF.pdf", {
+      subjectType: "drag-n-drop",
+    });
+    cy.wait(5000);
+    cy.get(
+      "#drag-drop-upload > .dz-preview > .dz-details > .dz-filename > span"
+    ).contains("oneMegaPDF");
+    /*     cy.get("#file-submit").click();
+    cy.get("div[class='example'] h3").should("have.text", "File Uploaded!");
+    cy.get("#uploaded-files").contains("myFile.pdf");
+ */
   });
-  it("Single File upload", () => {
-    cy.visit("https://the-internet.herokuapp.com/upload");
-    cy.get("#file-upload");
+  it("Multiple files upload", () => {
+    cy.visit("https://davidwalsh.name/demo/multiple-file-upload.php");
+    cy.get("#filesToUpload").attachFile(["oneMegaPDF.pdf", "deuxMegasPDF.pdf"]);
+    cy.wait(3000);
+    cy.get(":nth-child(6) > strong").should(
+      "contain.text",
+      "Files You Selected:"
+    );
+    cy.get("ul[id='fileList'] li").should("contain.text", "oneMegaPDF.pdf");
+  });
+
+  it.only("File upload - Shadow DOM", () => {
+    cy.visit(
+      "https://www.htmlelements.com/demos/fileupload/shadow-dom/index.htm"
+    );
+    cy.get(".smart-browse-input", { includeShadowDom: true }).attachFile(
+      "deuxMegasPDF.pdf"
+    );
+    cy.wait(5000);
+    cy.get(".smart-item-name", { includeShadowDom: true }).contains(
+      "deuxMegasPDF.pdf"
+    );
   });
 });
