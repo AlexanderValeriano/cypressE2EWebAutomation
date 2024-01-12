38e941515452bdcd24e194313fdd9f1a460c5b0f
valeriano alexander <valerianoalexander@gmail.com>
2024-01-12 15:42:53 +0100

    18. Browser Navigation | go() & reload()

---
+++ cypress/e2e/Navigation.cy.js
@@ -0,0 +1,21 @@
+describe("mySuite", () => {
+  it.only("NavigationsTest", () => {
+    cy.visit("https://demo.opencart.com/");
+    cy.title().should("eq", "Your Store"); // Home page
+    cy.get(":nth-child(7) > .nav-link").click();
+    cy.get("div[id='content'] h2").should("have.text", "Cameras"); // cameras page
+    cy.go("back");
+    cy.title().should("eq", "Your Store"); // go back to home
+
+    cy.go("forward");
+    cy.get("div[id='content'] h2").should("have.text", "Cameras"); //cameras
+
+    cy.go(-1);
+    cy.title().should("eq", "Your Store"); // Home page
+
+    cy.go(1); // cameras
+    cy.get("div[id='content'] h2").should("have.text", "Cameras"); //cameras
+
+    cy.reload();
+  });
+});
